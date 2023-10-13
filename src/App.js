936fea338a154c2001f21e6406faadd5f391b395
gabriel roux <rouxgab81@gmail.com>
2023-10-13 11:06:11 -0400

    fix formSplitBill with key

--- src/App.js
+++ src/App.js
@@ -75,6 +75,7 @@ const App = () => {
         <FormSplitBill 
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
+          key={selectedFriend.id}
         />
       }
 
